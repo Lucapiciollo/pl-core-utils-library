a8063f047ce3b6ecd3e67e340c270e774970e275
Luca Piciollo <lucapiciollo@gmail.com>
2026-05-14 23:11:02 +0200

    fix

--- projects/pl-core-utils-library/src/lib/pl-core-utils-library.module.ts
+++ projects/pl-core-utils-library/src/lib/pl-core-utils-library.module.ts
@@ -1,94 +1,116 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { NgModule, OnDestroy, Optional } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NavigationStart, Router } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { Subject } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
- import { BROWSER, BROWSER_VALID, DISABLE_LOG, PlAmbientModeLoaderService } from './service/pl-ambient-mode.service';
+
+import {
+  BROWSER,
+  BROWSER_VALID,
+  DISABLE_LOG,
+  PlAmbientModeLoaderService
+} from './service/pl-ambient-mode.service';
 import { CACHE_TAG, MAX_CACHE_AGE } from './service/pl-cache-map.service';
-import { DEFAULT_PATH_MOCK, PlHttpInterceptorMockService } from './interceptor/pl-interceptor-mock.service';
+import {
+  DEFAULT_PATH_MOCK,
+  PlHttpInterceptorMockService
+} from './interceptor/pl-interceptor-mock.service';
 import { AlertComponent } from './component/alert/alert.component';
 import { AlertService } from './component/alert/alert.service';
 import { PlBaseComponent } from './component/base-component/pl-base-component.component';
 
-
-
 @NgModule({
-  declarations: [  AlertComponent, PlBaseComponent],
+  declarations: [AlertComponent, PlBaseComponent],
   imports: [
     CommonModule,
     FormsModule,
     HttpClientModule,
-    NgbModule ,
+    NgbModule
   ],
-  exports: [HttpClientModule, PlBaseComponent,CommonModule],
+  exports: [
+    HttpClientModule,
+    PlBaseComponent,
+    CommonModule
+  ],
   providers: [
     PlAmbientModeLoaderService,
+
     /**
-     * @author l.piciollo
-     * configurazione per il servizio dei caching
+     * Configurazione per il servizio di caching.
      */
     { provide: BROWSER_VALID, useValue: [BROWSER.ALL] },
     { provide: DISABLE_LOG, useValue: false },
     { provide: MAX_CACHE_AGE, useValue: 300000 },
-    { provide: CACHE_TAG, useValue: "@cachable@" },
-    /**
-     * configurazione intercettore per servizi mock
-     */
-    { provide: HTTP_INTERCEPTORS, useClass: PlHttpInterceptorMockService, multi: true },
-    /**
-     * inizializzazione del path per reperire gli stub json di risposta al mock 
-     */
-    { provide: DEFAULT_PATH_MOCK, useValue: "public/mock" }
+    { provide: CACHE_TAG, useValue: '@cachable@' },
 
-    //{ provide: APP_INITIALIZER, useFactory: PlAmbientModeLoaderService, deps: [ ], multi: true }
+    /**
+     * Configurazione intercettore per servizi mock.
+     */
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: PlHttpInterceptorMockService,
+      multi: true
+    },
+
+    /**
+     * Path per reperire gli stub JSON di risposta al mock.
+     */
+    { provide: DEFAULT_PATH_MOCK, useValue: 'public/mock' }
   ]
-  //, entryComponents: [ AlertComponent]
 })
-/**
- * @author l.piciollo
- * modulo di inizializzazione della libreria, occorre importare questo modulo affinche le funzionalita vengano attivate.
- * il modulo esporta una funzionalità per reperire un interrupt da passare alle http in caso di cambio rotta
- * @example 
- *       imports :[PlCoreModule]
- *       providers: [{ provide: APP_INITIALIZER, useFactory: PlAmbientModeLoaderService, deps: [ ], multi: true }]
- * 
- *   per reperire il cambio rotta
- *       PlCoreModule.Routing().getIinterrupt()   
- */
-export class PlCoreModule {
+export class PlCoreModule implements OnDestroy {
   private static interrupter: Subject<boolean> = new Subject<boolean>();
+
+  private routeEventsSubscription: Subscription | null = null;
+
   /**
-   * al momento della costruzione del modulo.. viene creato un intercettore di cambio rotta NavigationStart
-   * @param plAmbientModeLoaderService 
-   * @param router 
+   * Modulo di inizializzazione della libreria.
+   *
+   * Uso:
+   *
+   * imports: [PlCoreModule]
+   *
+   * Per reperire il cambio rotta:
+   *
+   * PlCoreModule.Routing().getIinterrupt()
    */
-  constructor(private alertService: AlertService, private plAmbientModeLoaderService: PlAmbientModeLoaderService, private router: Router  ) {
+  constructor(
+    private alertService: AlertService,
+    private plAmbientModeLoaderService: PlAmbientModeLoaderService,
+    @Optional() private router?: Router
+  ) {
     try {
-
- 
-
       this.alertService.enableAlertMessage(true);
-      
-      router.events.pipe(filter(event => event instanceof NavigationStart)).subscribe(event => {
-        PlCoreModule.interrupter.next(true)
-      });
+
+      this.routeEventsSubscription =
+        this.router?.events
+          .pipe(filter(event => event instanceof NavigationStart))
+          .subscribe(() => {
+            PlCoreModule.interrupter.next(true);
+          }) ?? null;
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   }
+
+  ngOnDestroy(): void {
+    this.routeEventsSubscription?.unsubscribe();
+    this.routeEventsSubscription = null;
+  }
+
   /**
-   * funzionalità per il reperimento del'intercettore di rotta.. in caso di NavigationStart, viene lanciato questo 
-   * subject. utile per terminare le richieste http
+   * Funzionalità per il reperimento dell'interrupt di rotta.
+   * In caso di NavigationStart viene lanciato il subject.
+   * Utile per terminare le richieste HTTP.
    */
-  static Routing() {
+  static Routing(): { getIinterrupt(): Subject<boolean> } {
     return {
       getIinterrupt(): Subject<boolean> {
         return PlCoreModule.interrupter;
       }
-    }
+    };
   }
-
 }
