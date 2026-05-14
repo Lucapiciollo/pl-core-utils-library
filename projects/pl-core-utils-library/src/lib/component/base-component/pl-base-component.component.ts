8bd2941ac953556fc0f1129bfbebd5bfaff802a1
Luca Piciollo <lucapiciollo@gmail.com>
2026-05-14 23:03:59 +0200

    fix

--- projects/pl-core-utils-library/src/lib/component/base-component/pl-base-component.component.ts
+++ projects/pl-core-utils-library/src/lib/component/base-component/pl-base-component.component.ts
@@ -3,98 +3,152 @@
  * @email lucapiciollo@gmail.com
  * @create date 2020-12-22 16:42:38
  * @modify date 2020-12-22 16:42:38
- * @desc [componente base per l'estensione di componenti.. utile per la condivisione di servizi e funzionalità
- * basilari comuni a tutti i componenti]
+ * @desc componente base per l'estensione di componenti.
+ * Utile per la condivisione di servizi e funzionalità basilari comuni a tutti i componenti.
  */
 
 import { HttpParams } from '@angular/common/http';
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Injector, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Injector,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ReplaySubject, Subscription } from 'rxjs';
+import { Delay, Log, Unsubscribe } from 'pl-decorator';
+
 import { PlGraphicService } from '../../service/pl-graphic.service';
 import { PlHttpService } from '../../service/pl-http.service';
 import { PlNetworkService } from '../../service/pl-network.service';
 import { PlUtilsService } from '../../service/pl-utils.service';
 import { PLWorkerService } from '../../service/pl-worker.service';
-import { Delay, Log, Unsubscribe } from 'pl-decorator';
-@Component({
-    template: "",
-    standalone: false
-})
 
-export class PlBaseComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+@Component({
+  template: '',
+  standalone: false
+})
+export class PlBaseComponent
+  implements
+  OnChanges,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   protected graphicService: PlGraphicService = null;
   protected httpService: PlHttpService = null;
   protected networkService: PlNetworkService = null;
   protected utilsService: PlUtilsService = null;
   protected workerService: PLWorkerService = null;
-  protected queryParams: ReplaySubject<any> = new ReplaySubject(1);
-  protected params: ReplaySubject<any> = new ReplaySubject(1);
-  protected data: ReplaySubject<any> = new ReplaySubject(1);
+
+  protected queryParams: ReplaySubject<any> = new ReplaySubject<any>(1);
+  protected params: ReplaySubject<any> = new ReplaySubject<any>(1);
+  protected data: ReplaySubject<any> = new ReplaySubject<any>(1);
+
   protected queryParamsObs: Subscription = null;
   protected paramsObs: Subscription = null;
   protected dataObs: Subscription = null;
+
   protected router: Router;
   protected route: ActivatedRoute;
 
   constructor(protected injector: Injector) {
     this.router = this.injector.get(Router);
     this.route = this.injector.get(ActivatedRoute);
+
     this.graphicService = this.injector.get(PlGraphicService);
     this.httpService = this.injector.get(PlHttpService);
     this.networkService = this.injector.get(PlNetworkService);
     this.utilsService = this.injector.get(PlUtilsService);
     this.workerService = this.injector.get(PLWorkerService);
+
     this.paramsObs = this.route.params.subscribe(param => {
-      this.params.next(param)
+      this.params.next(param);
     });
+
     this.queryParamsObs = this.route.queryParams.subscribe(param => {
       this.queryParams.next(param);
     });
+
     this.dataObs = this.route.data.subscribe(param => {
-      this.data.next(param)
+      this.data.next(param);
     });
   }
 
-
   /**
-  *  @author @l.piciollo 
-   * @param pageUrl      Url della pagina di destinazione
-   * @param extras       Oggetto contenente direttire di rotta
-   * @param queryParams  parametri da passare alla pagina
+   * @author @l.piciollo
+   * @param pageUrl Url della pagina di destinazione
+   * @param extras Oggetto contenente direttive di rotta
+   * @param queryParams parametri da passare alla pagina
    * @description funzione che si occupa di richiamare una pagina e di passare parametri per query url.
-   * è possibile passare anche json complessi, il tutto viene annidato in un unico oggetto e convertito in base64
-   * per essere trasportato.
+   * È possibile passare anche json complessi.
    */
-  goToPage(pageUrl: string, extras = {}, queryParams = {}, preservedQueryParams: Array<string> = []) {
+  goToPage(
+    pageUrl: string,
+    extras: Record<string, any> = {},
+    queryParams: Record<string, any> = {},
+    preservedQueryParams: string[] = []
+  ): void {
     try {
-      queryParams != null ? null : queryParams = {};
+      queryParams = queryParams ?? {};
+
       const url = window.location.href;
-      const httpParams = new HttpParams({ fromString: url.split('?')[1] });
-      preservedQueryParams.forEach(k => {
-        if (!queryParams.hasOwnProperty(k)) {
-          queryParams[k] = httpParams.get(k)
+      const httpParams = new HttpParams({
+        fromString: url.split('?')[1]
+      });
+
+      preservedQueryParams.forEach(key => {
+        if (!Object.prototype.hasOwnProperty.call(queryParams, key)) {
+          queryParams[key] = httpParams.get(key);
         }
-      })
-      if (extras == undefined || extras == null) extras = {};
-      if (queryParams != null) {
-        Object.assign(extras, { queryParams: queryParams })
-      }
-      this.router.navigate([pageUrl], extras).then(redirected => { });
+      });
+
+      extras = extras ?? {};
+
+      Object.assign(extras, {
+        queryParams
+      });
+
+      this.router.navigate([pageUrl], extras).then(() => { });
     } catch (e) {
       throw e;
     }
   }
 
+  @Log('debug')
+  ngOnInit(): void { }
 
+  @Log('debug')
+  ngAfterContentInit(): void { }
 
+  @Log('debug')
+  ngOnDestroy(): void {
+    this.queryParamsObs?.unsubscribe();
+    this.paramsObs?.unsubscribe();
+    this.dataObs?.unsubscribe();
 
-  @Log('debug') ngOnInit() { }
-  @Log('debug') ngAfterContentInit() { }
-  @Log('debug') ngOnDestroy() { }
-  ngAfterViewInit() { }
-  ngAfterViewChecked() { }
-  ngAfterContentChecked() { }
-  ngDoCheck() { }
-  ngOnChanges(changes: SimpleChanges) { }
+    this.queryParams.complete();
+    this.params.complete();
+    this.data.complete();
+  }
+
+  ngAfterViewInit(): void { }
+
+  ngAfterViewChecked(): void { }
+
+  ngAfterContentChecked(): void { }
+
+  ngDoCheck(): void { }
+
+  ngOnChanges(changes: SimpleChanges): void { }
 }
