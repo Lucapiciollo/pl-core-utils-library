a089ed3bc324f310863ebcd845fba1e3250475e4
Luca Piciollo <lucapiciollo@gmail.com>
2026-05-14 22:57:05 +0200

    fix

--- projects/pl-core-utils-library/src/public-api.ts
+++ projects/pl-core-utils-library/src/public-api.ts
@@ -2,18 +2,31 @@
  * Public API Surface of pl-core-utils-library
  */
 
+// Module
 export { PlCoreModule } from './lib/pl-core-utils-library.module';
+
+// Components
+export { AlertComponent } from './lib/component/alert/alert.component';
+export { PlBaseComponent } from './lib/component/base-component/pl-base-component.component';
+
+// Services
+export { AlertService } from './lib/component/alert/alert.service';
+export { PlAmbientModeLoaderService, BROWSER, BROWSER_VALID, DISABLE_LOG } from './lib/service/pl-ambient-mode.service';
 export { MAX_CACHE_AGE, CACHE_TAG, PlCacheMapService } from './lib/service/pl-cache-map.service';
-export { PlAmbientModeLoaderService, BROWSER, BROWSER_VALID, DISABLE_LOG } from './lib/service/pl-ambient-mode.service'
 export { PlGraphicService } from './lib/service/pl-graphic.service';
+export { PlHttpService, RESPONSE_TYPE, CONTENT_TYPE, TYPE_EVENT_NETWORK } from './lib/service/pl-http.service';
+export { PlNetworkService } from './lib/service/pl-network.service';
 export { PlUtilsService } from './lib/service/pl-utils.service';
 export { PLWorkerService } from './lib/service/pl-worker.service';
-export { PlNetworkService } from "./lib/service/pl-network.service"
-export { PlCoreUtils, progressBarsInterface } from './lib/pl-core-utils-library.service';
-export { PlHttpService, RESPONSE_TYPE, CONTENT_TYPE, TYPE_EVENT_NETWORK } from './lib/service/pl-http.service';
-export { AlertComponent } from './lib/component/alert/alert.component';
-export { AlertService } from './lib/component/alert/alert.service';
-export { PLPermission, PLTraceHooks,   TYPE_EVENT, PLTraceMethod  } from "./lib/decorator/decordator";
+
+// Interceptors
 export { DEFAULT_PATH_MOCK, PlHttpInterceptorMockService } from './lib/interceptor/pl-interceptor-mock.service';
-export { PlHttpRequest } from "./lib/bean/Pl-http-request";
-export { PlBaseComponent } from "./lib/component/base-component/pl-base-component.component";
+
+// Utils
+export { PlCoreUtils, progressBarsInterface } from './lib/pl-core-utils-library.service';
+
+// Decorators
+export { PLPermission, PLTraceHooks, TYPE_EVENT, PLTraceMethod } from './lib/decorator/decordator';
+
+// Beans
+export { PlHttpRequest } from './lib/bean/Pl-http-request';
