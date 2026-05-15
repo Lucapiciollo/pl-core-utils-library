5a5fb0b9794fc8b8e298cb75194df33d534080fd
Luca Piciollo <lucapiciollo@gmail.com>
2026-05-15 10:33:07 +0200

    chore: update public api exports

--- projects/pl-core-utils-library/src/public-api.ts
+++ projects/pl-core-utils-library/src/public-api.ts
@@ -4,38 +4,46 @@
 
 // Module
 export {
-    PlCoreModule,
-    PlCoreModuleConfig,
-    PL_CORE_MODULE_CONFIG
+  PlCoreModule,
+  PlCoreModuleConfig,
+  PL_CORE_MODULE_CONFIG
 } from './lib/pl-core-utils-library.module';
+
 // Components
 export { AlertComponent } from './lib/component/alert/alert.component';
 export { PlBaseComponent } from './lib/component/base-component/pl-base-component.component';
 
 // Services
 export { AlertService } from './lib/component/alert/alert.service';
-export { PlAmbientModeLoaderService, BROWSER, BROWSER_VALID, DISABLE_LOG } from './lib/service/pl-ambient-mode.service';
-export { MAX_CACHE_AGE, CACHE_TAG, PlCacheMapService } from './lib/service/pl-cache-map.service';
+export {
+  PlAmbientModeLoaderService,
+  BROWSER,
+  BROWSER_VALID,
+  DISABLE_LOG
+} from './lib/service/pl-ambient-mode.service';
+export {
+  MAX_CACHE_AGE,
+  CACHE_TAG,
+  PlCacheMapService
+} from './lib/service/pl-cache-map.service';
 export { PlGraphicService } from './lib/service/pl-graphic.service';
-export { PlHttpService, RESPONSE_TYPE, CONTENT_TYPE, TYPE_EVENT_NETWORK } from './lib/service/pl-http.service';
+export {
+  PlHttpService,
+  RESPONSE_TYPE,
+  CONTENT_TYPE,
+  TYPE_EVENT_NETWORK
+} from './lib/service/pl-http.service';
 export { PlNetworkService } from './lib/service/pl-network.service';
 export { PlUtilsService } from './lib/service/pl-utils.service';
 export { PLWorkerService } from './lib/service/pl-worker.service';
 
 // Interceptors
-export { DEFAULT_PATH_MOCK, PlHttpInterceptorMockService } from './lib/interceptor/pl-interceptor-mock.service';
- 
-
-// Decorators
-export { PLPermission, PLTraceHooks, TYPE_EVENT, PLTraceMethod } from './lib/decorator/decordator';
-
-// Beans
 export {
-    PlHttpRequest,
-    PlHttpMethod,
-    PlHttpRequestConfig
-} from './lib/bean/Pl-http-request';
+  DEFAULT_PATH_MOCK,
+  PlHttpInterceptorMockService
+} from './lib/interceptor/pl-interceptor-mock.service';
 
+// Utils
 export {
   PlCoreUtils,
   PlProgressBar,
@@ -43,3 +51,20 @@ export {
   progressBarsInterface,
   PlBroadcastEventListener
 } from './lib/pl-core-utils-library.service';
+
+export { createPlUuid } from './lib/utils/pl-uuid.util';
+
+// Decorators
+export {
+  PLPermission,
+  PLTraceHooks,
+  TYPE_EVENT,
+  PLTraceMethod
+} from './lib/decorator/decordator';
+
+// Beans
+export {
+  PlHttpRequest,
+  PlHttpMethod,
+  PlHttpRequestConfig
+} from './lib/bean/Pl-http-request';
