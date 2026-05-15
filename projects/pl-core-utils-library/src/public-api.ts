44bbb20ed3f3c470163dd6583b1501363c9bc609
Luca Piciollo <lucapiciollo@gmail.com>
2026-05-15 12:16:11 +0200

    chore: update final public api exports

--- projects/pl-core-utils-library/src/public-api.ts
+++ projects/pl-core-utils-library/src/public-api.ts
@@ -15,31 +15,42 @@ export { PlBaseComponent } from './lib/component/base-component/pl-base-componen
 
 // Services
 export { AlertService } from './lib/component/alert/alert.service';
+
 export {
   PlAmbientModeLoaderService,
   BROWSER,
   BROWSER_VALID,
-  DISABLE_LOG
+  DISABLE_LOG,
+  PlAmbientModeResult
 } from './lib/service/pl-ambient-mode.service';
+
 export {
   MAX_CACHE_AGE,
   CACHE_TAG,
   PlCacheMapService,
   PlCacheItem
 } from './lib/service/pl-cache-map.service';
+
 export { PlGraphicService } from './lib/service/pl-graphic.service';
+
 export {
   PlHttpService,
   RESPONSE_TYPE,
   CONTENT_TYPE,
   TYPE_EVENT_NETWORK
 } from './lib/service/pl-http.service';
- 
+
+export {
+  PlNetworkService,
+  PlLocalHttpInfo
+} from './lib/service/pl-network.service';
+
 export {
   PlUtilsService,
   PlWindowSize,
   PlBinaryFindInput
 } from './lib/service/pl-utils.service';
+
 export { PLWorkerService } from './lib/service/pl-worker.service';
 
 // Interceptors
@@ -73,8 +84,3 @@ export {
   PlHttpMethod,
   PlHttpRequestConfig
 } from './lib/bean/Pl-http-request';
-
-export {
-  PlNetworkService,
-  PlLocalHttpInfo
-} from './lib/service/pl-network.service';
